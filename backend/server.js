8426a3903ce78de7539b838ed4df53d69c11e17b
Ogungbade Shalom <badeshalom@gmail.com>
2025-11-19 14:16:42 +0100

    fix;Ui update

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,6 @@ app.use('/api/auth', authRoutes);
 app.use('/api/complaints', complaintRoutes);
 
 // ✅ 'alter: true' updates the existing tables without deleting data
-sequelize.sync({ alter: true }).then(() => {
+sequelize.sync({ }).then(() => {
   app.listen(5000, () => console.log('Server running on port 5000'));
 });
